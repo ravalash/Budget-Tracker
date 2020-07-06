a45c716ac5ccad23c42394df64b26b6414753720
Ravalash <13k0873juhky@opayq.net>
2020-07-06 15:35:40 -0400

    Fixing env variables

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config();
+const path = require("path");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_HOST, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
